1c63bd26e5954a7a1c7260303ded1df769849942
Loong Yong En <loong@portkey.finance>
2024-01-03 13:09:31 +0800

    feat: connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { IPortkeyProvider, MethodsBase } from "@portkey/provider-types";
+import "./App.css";
+import detectProvider from "@portkey/detect-provider";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [provider, setProvider] = useState<IPortkeyProvider | null>(null);
+
+  const init = async () => {
+    try {
+      setProvider(await detectProvider());
+    } catch (error) {
+      console.log(error, "=====error");
+    }
+  };
+
+  const connect = async () => {
+    await provider?.request({
+      method: MethodsBase.REQUEST_ACCOUNTS,
+    });
+  };
+
+  useEffect(() => {
+    if (!provider) init();
+  }, [provider]);
+
+  if (!provider) return <>Provider not found.</>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={connect}>Connect</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
